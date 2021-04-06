8ed91f9eea719b9f9424723d1248dfeec7872035
necmigunduz <necmigunduz@gmail.com>
2021-04-06 16:45:56 +0300

    Connect form to table

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let myLibrary = [];
 
-function Book(title, author, publication_year, city) {
+function Book(title, author, p_year, city) {
   this.title = title;
   this.author =author;
-  this.publication_year;
+  this.p_year;
   this.city = city;
 }
 
@@ -23,4 +23,15 @@ for (var i = 0; i < coll.length; i++) {
       content.style.display = "block";
     }
   });
+}
+
+function display () {
+  document.getElementById("d_area_1").innerHTML = document.getElementById("title").value;
+  document.getElementById("title").value = "";
+  document.getElementById("d_area_2").innerHTML = document.getElementById("author").value;
+  document.getElementById("author").value = "";
+  document.getElementById("d_area_3").innerHTML = document.getElementById("p_year").value;
+  document.getElementById("p_year").value = "";
+  document.getElementById("d_area_4").innerHTML = document.getElementById("city").value;
+  document.getElementById("city").value = "";
 }
