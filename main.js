0f71ec0de52121f06c67afd6075bb458bd15f833
necmigunduz <necmigunduz@gmail.com>
2021-04-06 18:42:06 +0300

    Update show

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const p_year = document.querySelector('#p_year');
 const city = document.querySelector('#city');
-// const button = document.querySelector('#btn');
-// const read = document.querySelector('#read');
 const form = document.querySelector('.form');
 const f_submit = document.querySelector('#f_submit');
 
@@ -17,9 +15,10 @@ function Book(title, author, p_year, city) {
   this.city = city;
 }
 
-// function addBookToLibrary(book) {
-//   myLibrary.push(book);
-// }
+function saveLibrary() {
+  localStorage.lib = JSON.stringify(myLibrary);
+}
+
 
 var coll = document.getElementsByClassName("collapsible");
 
@@ -35,57 +34,36 @@ for (var i = 0; i < coll.length; i++) {
   });
 }
 
+function displayBooks(){
+  container.innerHTML = "";
+
+  for(let i= 0; i<library.length; i++){
+    const content = document.createElement('div');
+    content.setAttribute('id',i);
+    const text = document.createElement('p');
+    text.textContent = library[i].info();
+
+    content.appendChild(text);
+    container.appendChild(content);
+  }
+}
+
 function addBookToLibrary(){
-
-  document.getElementByClassName('content').addEventListener('Submit', (e) => {
-
-   e.preventDefault();
-
-   let title = document.getElementById('title').value;
-   
-   let author = document.getElementById('author').value;
-   let p_year = document.getElementById('p_year').value;
-   let city = document.getElementById('city').value;
-   let book = new Book(title, author, p_year, city);
-   console.log(author)
-  addBookToTable(book);
-  myLibrary.push(book);
- })
+  const newBook = new Book(title.value, author.value, p_year.value, city.value);
+  myLibrary.push(newBook);
+  saveLibrary();
+  displayBooks();
 }
 
-function addBookToTable(book) {
-  const table = document.getElementById('t_body')
-  const row = document.createElement('tr')
-  row.innerHTML = `
-  <td>${book.title}</td>
-  <td>${book.author}</td>
-  <td>${book.p_year}</td>
-  <td>${book.city}</td>`;
-  table.appendChild(row);
+function loadLibrary() {
+  const books = JSON.parse(localStorage.lib);
+  for (let i = 0; i < books.length; i += 1) {
+    Object.setPrototypeOf(books[i], Book.prototype);
+  }
+  return books;
 }
 
-
-let book1 = {
-  title:'Critical Thinking',
-  author: 'Jon',
-  p_year: 2035,
-  city: 'Camer'
+if (localStorage.lib) {
+  library = loadLibrary();
+  showBooks();
 }
-let book2 = {
-  title:'Critical Thinking',
-  author: 'Jon',
-  p_year: 2035,
-  city: 'Camer'
-}
-let book3 = {
-  title:'Critical Thinking',
-  author: 'Jon',
-  p_year: 2035,
-  city: 'Camer'
-}
-// addBookToTable(book)
-for(let book in myLibrary){
-  // console.log(myLibrary[book])
-  addBookToTable(myLibrary[book]);
-}
-
