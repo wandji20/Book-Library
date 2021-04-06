a25879cfff2629307ab0a139d6f1c6b3b0cababf
necmigunduz <necmigunduz@gmail.com>
2021-04-06 16:29:10 +0300

    Add button and form to add new books

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ function Book(title, author, publication_year, city) {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
+
+var coll = document.getElementsByClassName("collapsible");
+
+for (var i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
