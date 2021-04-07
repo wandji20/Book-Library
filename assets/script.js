4f05d2ed751925ec8913194b4b280e07cb69cb2a
necmigunduz <necmigunduz@gmail.com>
2021-04-07 14:01:04 +0300

    Add change read function

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,11 @@ const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const read = document.querySelector('#read');
-const notRead = document.querySelector('#not-read');
-// const readCheck = document.getElementsByName('read-check') 
+// const notRead = document.querySelector('#not-read');
+// const read = document.querySelectorAll('input[name="read-check"]'); 
 const button = document.querySelector('#btn');
 const form = document.querySelector('form');
 const newBookBtn = document.querySelector('#form');
-
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -18,14 +17,24 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+
+
 Book.prototype.info = function () {
-  
-  return `${this.title} by ${this.author}, ${this.pages} , Read: ${this.read} `;
+  const readAlready = (this.read) ? "This book is already read!" : "This book is not read yet!";
+  return `${this.title} by ${this.author}, ${this.pages} , ${readAlready}`;
 };
 
 function saveLibrary() {
   localStorage.lib = JSON.stringify(library);
 }
+
+function changeRead() {
+  const {id} = this.parentNode;
+  const paragraph = this.parentNode.querySelector('p');
+  library[id].read = !library[id].read;
+  paragraph.innerHTML = library[id].info();
+  saveLibrary();
+} 
 
 function showBooks() {
   container.innerHTML = '';
@@ -35,6 +44,15 @@ function showBooks() {
     const text = document.createElement('p');
     text.textContent = library[i].info();
     content.appendChild(text);
+    container.appendChild(content);
+
+    const changeReadBtn = document.createElement("button");
+    changeReadBtn.addEventListener('click', changeRead);
+    changeReadBtn.textContent = "Change read status";
+
+    content.appendChild(text);
+    content.appendChild(changeReadBtn);
+    // content.appendChild(removeBtn);
     container.appendChild(content);
   }
 }
@@ -60,14 +78,6 @@ function loadLibrary() {
   }
   return books;
 }
-
-// function changeRead() {
-//   const { id } = this.parentNode;
-//   const para = this.parentNode.querySelector('p');
-//   library[id].read = !library[id].read;
-//   para.innerHTML = library[id].info();
-//   saveLibrary();
-// }
 
 if (localStorage.lib) {
   library = loadLibrary();
