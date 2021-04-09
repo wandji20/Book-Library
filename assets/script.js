168006b743669256b576e9004cf626c93f27f708
wandji <wbertrand10@gmail.com>
2021-04-09 11:08:11 +0100

    Add Book class

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,27 @@
-
-const container = document.querySelector('.container');
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const pages = document.querySelector('#pages');
-const read = document.querySelector('#read');
 const button = document.querySelector('#btn');
 const form = document.querySelector('form');
 const newBookBtn = document.querySelector('#form');
 
 
-const bookFactory = (title, author, pages, read) => ({
-  title, author, pages, read,
-});
+
+class Book{
+  constructor(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+
+
+
+
+
+
+// const bookFactory = (title, author, pages, read) => ({
+//   title, author, pages, read,
+// });
 
 const info = (book) => {
   const readAlready = book.read ? 'This book is already read!' : 'This book is not read yet!';
@@ -20,15 +30,16 @@ const info = (book) => {
 
 const library = (() => {
   let mylibrary = [];
+
   function saveLibrary() {
     localStorage.lib = JSON.stringify(mylibrary);
   }
-
+  
   function loadLibrary() {
     const books = JSON.parse(localStorage.lib);
     return books;
   }
-
+  
   function changeRead() {
     mylibrary = loadLibrary();
     const { id } = this.parentNode;
@@ -37,17 +48,18 @@ const library = (() => {
     saveLibrary();
     paragraph.innerHTML = info(loadLibrary()[id]);
   }
-
+  
   function removeBook() {
     mylibrary = loadLibrary();
-
+    
     const { id } = this.parentNode;
     mylibrary.splice(id, 1);
     saveLibrary(); // eslint-disable-next-line no-use-before-define
     showBooks();
   }
-
+  
   function showBooks() {
+    const container = document.querySelector('.container');
     container.innerHTML = '';
     for (let i = 0; i < loadLibrary().length; i += 1) {
       const content = document.createElement('div');
@@ -56,25 +68,29 @@ const library = (() => {
       text.textContent = info(loadLibrary()[i]);
       content.appendChild(text);
       container.appendChild(content);
-
+      
       const changeReadBtn = document.createElement('button');
       changeReadBtn.addEventListener('click', changeRead);
       changeReadBtn.textContent = 'Change read status';
-
+      
       const removeBtn = document.createElement('button');
       removeBtn.addEventListener('click', removeBook);
       removeBtn.textContent = 'Remove Book';
-
+      
       content.appendChild(text);
       content.appendChild(changeReadBtn);
       content.appendChild(removeBtn);
       container.appendChild(content);
     }
   }
-
-
+  
+  
   function addBookToLibrary() {
-    const newBook = bookFactory(title.value, author.value, pages.value, read.value);
+    const title = document.querySelector('#title');
+    const author = document.querySelector('#author');
+    const pages = document.querySelector('#pages');
+    const read = document.querySelector('#read');
+    const newBook = new Book(title.value, author.value, pages.value, read.value);
 
     mylibrary.push(newBook);
 
@@ -84,8 +100,7 @@ const library = (() => {
 
 
   return {
-    saveLibrary, showBooks, loadLibrary, addBookToLibrary, changeRead, removeBook,
-  };
+    saveLibrary, showBooks, loadLibrary, addBookToLibrary, changeRead, removeBook};
 })();
 
 function showForm() {
